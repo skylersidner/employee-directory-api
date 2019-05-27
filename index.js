ab678e7579e9f75d3a3e16727828a2932cbaf992
Skyler Sidner <skyler.sidner@atssecured.com>
2019-05-27 13:24:59 -0500

    change: adding environment variables for port and server environment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("It's working!!!");
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+});
+
+
+const port = process.env.PORT || 3000;
+const environment = process.env.NODE_ENV;
+app.listen(port, () => {
+    console.log(`Environment: ${environment}`)
+    console.log(`Listening on port ${port}...`);
+});
